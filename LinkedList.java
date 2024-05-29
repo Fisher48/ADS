dd3847f7f6e0cc26a9d8ff0f5b3dd8d7b931d52c
Fisher48 <rybakov48@yandex.ru>
2024-05-29 21:29:30 +0300

    Added LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,156 @@
+import java.util.*;
+
+public class LinkedList {
+    public Node head;
+    public Node tail;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void addInTail(Node item) {
+        if (this.head == null) {
+            this.head = item;
+        } else {
+            this.tail.next = item;
+        }
+        this.tail = item;
+    }
+
+    public Node find(int value) {
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public ArrayList<Node> findAll(int _value) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        // здесь будет ваш код поиска всех узлов
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == _value) {
+                nodes.add(node);
+            }
+            node = node.next;
+        }
+        return nodes;
+    }
+
+    public boolean remove(int _value) {
+        // здесь будет ваш код удаления одного узла по заданному значению
+        Node temp = find(_value);
+        boolean change = false;
+        if (temp == null) {
+            return false;
+        }
+        Node node = head;
+        while (node != null) {
+            if (temp == node) {
+                head = temp.next;
+                change = true;
+            }
+            if (temp == node.next) {
+                node.next = temp.next;
+                change = true;
+            }
+            if (this.head == null) {
+                this.tail = null;
+            }
+            if (change) {
+                return true;
+            }
+            node = node.next;
+        }
+        return change; // если узел не был удалён
+    }
+
+    public void removeAll(int _value) {
+        // здесь будет ваш код удаления всех узлов по заданному значению
+        Node temp = find(_value);
+        if (temp == null) {
+            return;
+        }
+        Node node = this.head;
+        while (node != null) {
+            if (temp == null) {
+                return;
+            }
+            if (temp == node) {
+                head = temp.next;
+                temp = find(_value);
+            }
+            if (this.head == null) {
+                this.tail = null;
+                return;
+            }
+            if (temp == node.next) {
+                if (temp == this.tail) {
+                    node.next = temp.next;
+                    this.tail = null;
+                    return;
+                }
+                node.next = temp.next;
+                temp = find(_value);
+            }
+            node = node.next;
+        }
+    }
+
+    public void clear() {
+        // здесь будет ваш код очистки всего списка
+        this.head = null;
+        this.tail = null;
+    }
+
+    public int count() {
+        // здесь будет ваш код подсчёта количества элементов в списке
+        Node node = this.head;
+        int length = 0;
+        while (node != null) {
+            length = length + 1;
+            node = node.next;
+        }
+        return length;
+    }
+
+    public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+        // здесь будет ваш код вставки узла после заданного
+        // если _nodeAfter = null ,
+        // добавьте новый элемент первым в списке
+        Node node = this.head;
+        if (_nodeAfter == null) {
+            head = _nodeToInsert;
+            _nodeToInsert.next = node;
+        }
+        while (node.next != null) {
+            if (this.tail == _nodeAfter) {
+                this.tail = _nodeAfter;
+                _nodeAfter.next = _nodeToInsert;
+                return;
+            }
+            if (node == _nodeAfter) {
+                node = _nodeAfter.next;
+                _nodeAfter.next = _nodeToInsert;
+                _nodeToInsert.next = node;
+            }
+            node = node.next;
+        }
+    }
+}
+
+class Node {
+    public int value;
+    public Node next;
+
+    public Node(int _value) {
+        value = _value;
+        next = null;
+    }
+}
+
