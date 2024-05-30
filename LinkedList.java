b48549bb93018075b2efa3b304de09915fa12383
Fisher48 <rybakov48@yandex.ru>
2024-05-30 22:13:50 +0300

    Added func sum

--- LinkedList.java
+++ LinkedList.java
@@ -142,6 +142,21 @@ public class LinkedList {
             node = node.next;
         }
     }
+
+    public static List<Integer> sum(LinkedList list1, LinkedList list2){
+        List<Integer> sumOfElements = new ArrayList<>();
+        if (list1.count() != list2.count()) {
+            return null;
+        }
+        Node node1 = list1.head;
+        Node node2 = list2.head;
+        while (node1 != null && node2 != null) {
+            sumOfElements.add(node1.value + node2.value);
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+        return sumOfElements;
+    }
 }
 
 class Node {
